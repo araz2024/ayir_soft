e178038fb6d05194e697fef2d1a2afea86bd2acb
Araz Ahmadov <araz.ahmadov2017@gmail.com>
2025-11-28 23:51:29 +0400

    Ayir_Soft

--- script.js
+++ script.js
@@ -38,14 +38,12 @@ const messages = {
 // =========================================
 
 const galleryImages = [
-    'images/gallery-1.jpg', 'images/gallery-2.jpg', 'images/gallery-3.jpg',
-    'images/gallery-4.jpg', 'images/gallery-5.jpg', 'images/gallery-6.jpg',
-    'images/gallery-7.jpg', 'images/gallery-8.jpg', 'images/gallery-9.jpg',
-    'images/gallery-10.jpg', 'images/gallery-11.jpg', 'images/gallery-12.jpg',
-    'images/gallery-13.jpg', 'images/gallery-14.jpg', 'images/gallery-15.jpg',
-    'images/gallery-16.jpg', 'images/gallery-17.jpg', 'images/gallery-18.jpg',
-    'images/gallery-19.jpg', 'images/gallery-20.jpg', 'images/gallery-21.jpg'
+    'images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg',
+    'images/6.jpg', 'images/7.jpg', 'images/8.jpg', 'images/9.jpg', 'images/10.jpg',
+    'images/11.jpg', 'images/12.jpg', 'images/13.jpg', 'images/14.jpg', 'images/15.jpg',
+    'images/16.jpg', 'images/18.jpg', 'images/19.jpg', 'images/20.jpg', 'images/21.jpg'
 ];
+
 
 let currentImageIndex = 0;
 
